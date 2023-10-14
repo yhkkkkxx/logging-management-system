62544c3769980145e87ad19727d739d1330c1ba5
yhkkkkxx <hayounglala@jbnu.ac.kr>
2023-10-14 21:44:06 +0900

    Initial commit

---
+++ src/main/java/capchelin/loggingManagementSystem/LoggingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package capchelin.loggingManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capchelin/loggingManagementSystem/LoggingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package capchelin.loggingManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
